4eb315a1da00111711287929065184930fc19019
akselele <ahamelryck@gmail.com>
2021-09-02 01:59:37 +0200

    frontend

---
+++ src/lolqueue_frontend/components/Name.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col col="12">
+        <v-card class="d-inline-block mx-auto pa-md-2 rounded-pill">
+          <v-container>
+            <v-row align="center">
+              <v-img
+                height="100"
+                width="100"
+                src="https://placekitten.com/200/200"
+                class="rounded-circle mr-md-4"
+                
+              ></v-img>
+              <v-col cols="auto" class="text-center pl-0">
+                <v-row class="flex-column ma-0 fill-height" justify="center">
+                  <v-col class="px-0"> Yuumi LFT </v-col>
+                  <v-col class="px-0"> Challenger --- 2000 LP</v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+</style>
