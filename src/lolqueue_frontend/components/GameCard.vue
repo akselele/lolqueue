8f37523f2ee392b4627cec5f522933cf14f9e15a
akselele <ahamelryck@gmail.com>
2021-10-08 11:20:35 +0200

    gap

--- src/lolqueue_frontend/components/GameCard.vue
+++ src/lolqueue_frontend/components/GameCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-row">
-    <div class="flex p-5 bg-gray-900 rounded-xl">
+    <div class="flex p-5 bg-gray-900 rounded-xl" :style="{ width: '450px' }">
       <div>
         <img class="w-20 h-20" :src="getChampIcon()" alt="Champion Played" />
       </div>
@@ -32,26 +32,26 @@
           </p>
         </div>
       </div>
-      <div class="flex flex-col my-auto ml-5">
-        <div class="flex flex-row">
+      <div class="flex flex-col my-auto ml-5 gap-2">
+        <div class="flex flex-row gap-2">
           <template v-for="item in playerData.items.slice(0, 3)">
             <img
               :key="item"
-              class="w-10 h-10"
+              class="w-10 h-10 ma-1"
               :src="getItemIcon(item)"
               alt="Item"
-              :style="[item === 0 ? { 'display':'none'} : {}]"
+              :style="[item === 0 ? { display: 'none' } : {}]"
             />
           </template>
         </div>
-        <div class="flex flex-row">
+        <div class="flex flex-row gap-2">
           <template v-for="item in playerData.items.slice(3, 6)">
             <img
               :key="item"
               class="w-10 h-10"
               :src="getItemIcon(item)"
               alt="Item"
-              :style="[item === 0 ? { 'display':'none'} : {}]"
+              :style="[item === 0 ? { display: 'none' } : {}]"
             />
           </template>
         </div>
